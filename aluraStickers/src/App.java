8061892fe6a25bf6c5149c868f25be8be1efebb8
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-03-30 00:06:43 -0300

    Change Conteudo from class to record

--- aluraStickers/src/App.java
+++ aluraStickers/src/App.java
@@ -8,13 +8,13 @@ public class App {
     public static final String ANSI_BOLD = "\u001b[1m";
 
     public static void main(String[] args) throws Exception {
-        // Fazer uma conexão HTTP e buscar os top 250 filmes
-        // String url =
-        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        // Fazer uma conexão HTTP e buscar os resultados
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-03-26&end_date=2022-03-28";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        // String url =
+        // "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-03-26&end_date=2022-03-28";
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
 
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
@@ -27,13 +27,13 @@ public class App {
         for (int i = 0; i < 3; i++) {
             Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
-            String nomeArquivo = conteudo.getTitulo() + ".png";
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = conteudo.titulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("Título: " + ANSI_BOLD + conteudo.getTitulo() + ANSI_RESET);
-            System.out.println("Imagem: " + ANSI_BOLD + conteudo.getUrlImagem() + ANSI_RESET);
+            System.out.println("Título: " + ANSI_BOLD + conteudo.titulo() + ANSI_RESET);
+            System.out.println("Imagem: " + ANSI_BOLD + conteudo.urlImagem() + ANSI_RESET);
 
             /*
              * System.out.println(ANSI_CYAN_BACKGROUND
--- aluraStickers/src/Conteudo.java
+++ aluraStickers/src/Conteudo.java
@@ -1,17 +1,24 @@
-public class Conteudo {
-    private final String titulo;
-    private final String urlImagem;
-
-    public Conteudo(String titulo, String urlImagem) {
-        this.titulo = titulo;
-        this.urlImagem = urlImagem;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getUrlImagem() {
-        return urlImagem;
-    }
+// RECORD VERSION
+public record Conteudo(String titulo, String urlImagem) {
 }
+
+// CLASS VERSION
+/*
+ * public class Conteudo {
+ * private final String titulo;
+ * private final String urlImagem;
+ * 
+ * public Conteudo(String titulo, String urlImagem) {
+ * this.titulo = titulo;
+ * this.urlImagem = urlImagem;
+ * }
+ * 
+ * public String getTitulo() {
+ * return titulo;
+ * }
+ * 
+ * public String getUrlImagem() {
+ * return urlImagem;
+ * }
+ * }
+ */
