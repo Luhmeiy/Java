fd573b369694443906fcf679462bc1fe1177f6af
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-09-23 18:38:48 -0300

    [spring-boot] Add Student class

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -1,11 +1,15 @@
 package com.example.springboot;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -15,7 +19,13 @@ public class Application {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Luhmeiy",
+						"luhmeiy@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21));
 	}
 }
